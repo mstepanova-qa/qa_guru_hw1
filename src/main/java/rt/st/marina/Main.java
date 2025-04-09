5a4e3fe00681521f45c357c97c98fa41f1c4d00c
Marina Stepanova <123195046+marinastepanova-qa@users.noreply.github.com>
2025-04-09 13:28:32 +0300

    comment

--- src/main/java/rt/st/marina/Main.java
+++ src/main/java/rt/st/marina/Main.java
@@ -3,6 +3,6 @@ package rt.st.marina;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hi, people!");
+        System.out.printf("Hi, people!!!");
     }
 }
