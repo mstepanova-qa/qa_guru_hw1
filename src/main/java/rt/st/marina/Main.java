93f9fc07ef70c64e6d92c5f2ba46e095c0fa9a88
Marina Stepanova <123195046+mstepanova-qa@users.noreply.github.com>
2025-04-09 13:53:22 +0300

    Update Main.java
    текст

--- src/main/java/rt/st/marina/Main.java
+++ src/main/java/rt/st/marina/Main.java
@@ -3,6 +3,6 @@ package rt.st.marina;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello, peopl!!!!!!!!!!");
+        System.out.printf("Hello, people!!!!!!!!!! hwu");
     }
 }
